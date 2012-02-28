fa88605ed7d698e74f1e073728fd012ed98b92c9
Fred Song <fsong@demonware.net>
2012-02-27 16:45:30 -0800

    more haskell notes on function syntax

--- haskell/fn-syntax.hs
+++ haskell/fn-syntax.hs
@@ -30,9 +30,11 @@ first (a,_,_) = a
 second (_,b,_) = b
 third (_,_,c) = c
 
+
 ----------------------------
 -- Pattern Matching
 ----------------------------
+-- <name> <pattern> = <expression>
 -- (x:xs) in the parameters is a common
 -- x is first element in list and xs is the rest
 head':: [a] -> a
@@ -62,3 +64,69 @@ capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 ----------------------------
 -- Guards
 ----------------------------
+bmiTell :: RealFloat a => a -> a -> String
+bmiTell weight height
+    | bmi <= skinny = "Underweight"
+    | bmi <= normal = "Normal"
+    | bmi <= fat = "Lose some weight"
+    | otherwise   = "Whale"
+    where bmi = weight / height^2 
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+calcBmis :: RealFloat a => [(a,a)] -> [a]
+calcBmis bmis = [bmi w h | (w,h) <- bmis]
+    where bmi weight height = weight / height^2
+
+-- NOTE: we can define functions using backtick syntax as well!
+myCompare :: Ord a => a -> a -> Ordering
+a `myCompare` b
+    | a > b     = GT
+    | a == b    = EQ 
+    | otherwise = LT
+
+-- We can use pattern matching in where
+initial :: String -> String -> String
+{-initial (f:_) (l:_) = [f] ++ "." ++ [l] ++ "."-}
+initial first last = [f] ++ "." ++ [l] ++ "."
+    where (f:_) = first
+          (l:_) = last
+
+----------------------------
+-- Let Bindings
+----------------------------
+-- let <bindings> in <expressions>
+cylinder :: RealFloat a => a -> a -> a
+cylinder r h = 
+    let sideArea = 2*pi*r
+        topArea = pi*r^2
+    in  sideArea + 2*topArea
+
+-- let vs. where:
+-- let is expression like if; where is syntactic construct
+{-4 * (let a = 9 in a + 1 ) + 2-}
+{-let sq x = x*x in (sq 3, sq 4, sq 5)-}
+{-(let a=1;b=2;c=3 in a*b*c, let foo="hey ";bar="there" in foo ++ bar)-}
+
+-- Using let expressions in list comprehensions
+-- We don't need the `in` for this case
+calcBmis' :: RealFloat a => [(a,a)] -> [a]
+calcBmis' bmis = [bmi | (w,h) <- bmis, let bmi=w/h^2, bmi>=25.0]
+
+
+----------------------------
+-- Case Expressions
+----------------------------
+-- case <expression> of (<pattern> -> <expression>)*
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ case xs of []  -> "empty"
+                                               [x] -> "a singleton"
+                                               xs  -> "a longer list"
+
+-- NOTE: <pattern> is like what's accepted in the function parameters
+describeList' :: [a] -> String
+describeList' xs = "The List is " ++ what xs
+    where what []   = "empty"
+          what [x]  = "a singleton"
+          what xs   = "a longer list"
+
