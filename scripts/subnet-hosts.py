b8be5695dede8f63a4e6eac965f752ba42964d35
Fred Song <fsong@demonware.net>
2012-03-19 14:07:27 -0700

    worked on subnet-hosts.py script

--- scripts/subnet-hosts.py
+++ scripts/subnet-hosts.py
@@ -1,4 +1,27 @@
-# pings and then lists all hosts on subnet
+# subnet-hosts.py, Fred Song
+# pings and lists all hosts in a subnet
 
-network_mask = 
+import sys
+import os
 
+def ip_up(ip, deadline=1):
+    return (os.system("ping -w%s %s > /dev/null" % (deadline,ip)) == 0)
+
+def ips_in_network(net_prefix, prefix_len):
+    #TODO: this is the hard part
+    pass
+
+if __name__ == "__main__":
+    if len(sys.argv) != 1:
+        print "usage: %s <network_prefix>/<prefix_length>" % sys.argv[0]
+        exit(1)
+
+    network = sys.argv[1].split('/')
+    net_prefix = network[0]
+    prefix_len = network[1]
+
+    print "Active ips:"
+    for ip in ips_in_network(net_prefix, prefix_len):
+        if ip_up(ip):
+            print ip
+    exit(0)
